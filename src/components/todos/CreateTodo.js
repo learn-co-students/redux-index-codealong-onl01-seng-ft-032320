ed907c7b28110d4fdc99ed3224361c1e47633a4c
Xearta <rsmcwow@gmail.com>
2020-08-04 10:46:34 -0400

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class CreateTodo extends Component {
-
   constructor() {
     super();
     this.state = {
@@ -12,30 +11,31 @@ class CreateTodo extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    this.props.addTodo(this.state)
-  }
+    this.props.addTodo(this.state);
+    this.setState({ text: '' });
+  };
 
   handleChange(event) {
     this.setState({
-      text: event.target.value
+      text: event.target.value,
     });
   }
 
   render() {
-    return(
+    return (
       <div>
         <form onSubmit={this.handleSubmit}>
-    	    <label>add todo</label>
-          <input type="text" onChange={(event) => this.handleChange(event)} value={this.state.text}/>
-          <input type="submit" />
-       </form>
-     </div>
-   );
+          <label>add todo</label>
+          <input type='text' onChange={event => this.handleChange(event)} value={this.state.text} />
+          <input type='submit' />
+        </form>
+      </div>
+    );
   }
-};
+}
 
 const mapDispatchToProps = dispatch => ({
-  addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData })
-})
+  addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData }),
+});
 
 export default connect(null, mapDispatchToProps)(CreateTodo);
